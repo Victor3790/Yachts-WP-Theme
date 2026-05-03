02e45e186ff50f9aaf477aeb6ee3cdb4e9d2ec5a
Victor <victor3790@gmail.com>
2026-05-02 19:06:09 -0600

    Constants added

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Functions and definitions
+ */
+
+define ( 'THEME_NAME', 'Yachts' );
+define ( 'THEME_URI', get_template_directory_uri() );
+define ( 'POSTS_PER_PAGE', 3 );
+define ( 'YACHTS_PER_PAGE', 5);
