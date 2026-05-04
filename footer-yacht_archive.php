e78466564e7a4f5f02dfc89d9a29e4fe509655df
Victor <victor3790@gmail.com>
2026-05-04 12:01:52 -0600

    Footer for yacht archive added

---
+++ footer-yacht_archive.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+	<footer class="footer section section--justified section--white">
+		<div class="footer__copy">
+			<p style="margin:0;">Copyright <?php echo date('Y'); ?> BARCOS BARCELONA S.L.</p>
+		</div>
+		<div class="footer__link">
+			<a style="color:#969696;" href="https://alquilerdeyatesenibiza.com" target="bank">
+				Alquiler de yates en ibiza
+			</a>
+		</div>
+  	</footer>
+</main>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
