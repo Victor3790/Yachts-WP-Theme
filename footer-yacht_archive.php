edb5e7fe3e74ade7ace8b9b348fa127e99bc7b46
Victor <victor3790@gmail.com>
2026-05-05 16:22:41 -0600

    Company name deleted in footer

--- footer-yacht_archive.php
+++ footer-yacht_archive.php
@@ -4,11 +4,11 @@
 
 	<footer class="footer section section--justified section--white">
 		<div class="footer__copy">
-			<p style="margin:0;">Copyright <?php echo date('Y'); ?> BARCOS BARCELONA S.L.</p>
+			<p style="margin:0;">Copyright <?php echo date('Y'); ?> YACHTS</p>
 		</div>
 		<div class="footer__link">
-			<a style="color:#969696;" href="https://alquilerdeyatesenibiza.com" target="bank">
-				Alquiler de yates en ibiza
+			<a style="color:#969696;" href="#" target="bank">
+				YACHTS
 			</a>
 		</div>
   	</footer>
